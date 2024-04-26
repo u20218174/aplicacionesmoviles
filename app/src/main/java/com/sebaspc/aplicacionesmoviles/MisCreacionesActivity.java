5dd203afa1a97532d7a06437381ff731e2cf128a
Sebastian Martin Peña Calderon <sebastianmartinpc@hotmail.com>
2024-04-26 01:00:44 -0500

    Correccion Activity

--- app/src/main/java/com/sebaspc/aplicacionesmoviles/MisCreacionesActivity.java
+++ app/src/main/java/com/sebaspc/aplicacionesmoviles/MisCreacionesActivity.java
@@ -2,13 +2,9 @@ package com.sebaspc.aplicacionesmoviles;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MisCreaciones extends AppCompatActivity {
+public class MisCreacionesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
