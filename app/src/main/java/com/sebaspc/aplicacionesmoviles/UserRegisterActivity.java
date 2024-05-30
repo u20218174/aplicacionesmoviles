7b2046acf67c5f6310080455595dec9409ca49d4
Sebaspc11 <U20218174@utp.edu.pe>
2024-05-30 11:47:05 -0500

    Correccion de Activities usando RelativeLayout

--- app/src/main/java/com/sebaspc/aplicacionesmoviles/UserRegisterActivity.java
+++ app/src/main/java/com/sebaspc/aplicacionesmoviles/UserRegisterActivity.java
@@ -1,6 +1,10 @@
 package com.sebaspc.aplicacionesmoviles;
 
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class UserRegisterActivity extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
+
+        textView = findViewById(R.id.txtTitulo);
+
+        Spannable text1 = new SpannableString("Regístrate y ");
+        text1.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.black)), 0,text1.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(text1);
+
+        Spannable text2 = new SpannableString("comienza a crear");
+        text2.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.ligh_green)), 0,text2.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.append(text2);
+
     }
 }
