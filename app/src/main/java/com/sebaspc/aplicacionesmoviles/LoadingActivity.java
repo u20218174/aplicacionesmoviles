e46f3f9f0c56e232d2082873e7101d82d396618f
oliver <u19303250@utp.edu.pe>
2024-05-31 21:23:15 -0500

    agregando animacion de splash con sebastian

--- app/src/main/java/com/sebaspc/aplicacionesmoviles/LoadingActivity.java
+++ app/src/main/java/com/sebaspc/aplicacionesmoviles/LoadingActivity.java
@@ -1,8 +1,10 @@
 package com.sebaspc.aplicacionesmoviles;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 import android.window.SplashScreen;
 import android.window.SplashScreenView;
 
@@ -14,11 +16,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoadingActivity extends AppCompatActivity {
 
+
+    ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_loading);
+
+        logo = findViewById(R.id.splash_image);
+
+        AnimatedVectorDrawable drawable = (AnimatedVectorDrawable) logo.getDrawable();
+        drawable.start();
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -29,6 +38,6 @@ public class LoadingActivity extends AppCompatActivity {
                 finish();
 
             }
-        }, 5000);
+        }, 5500);
     }
 }
