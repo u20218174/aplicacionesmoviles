7596e1b0d79e29c4a844494bdf487abe5d0483ea
Sebastian Martin Peña Calderon <sebastianmartinpc@hotmail.com>
2024-07-05 19:36:19 -0500

    Arreglando visualizacion de datos

--- app/src/main/java/com/sebaspc/aplicacionesmoviles/LoginActivity.java
+++ app/src/main/java/com/sebaspc/aplicacionesmoviles/LoginActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
@@ -18,17 +19,40 @@ public class LoginActivity extends AppCompatActivity {
 
     Button buton1;
     Button buton2;
+    EditText correo;
+    EditText contra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         buton1 = findViewById(R.id.btnProcesar);
 
+        correo = findViewById(R.id.etUsuario);
+        contra = findViewById(R.id.etContra);
+
         buton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+
+                Validador validador = new Validador();
+                if (validador.validar_correo(correo.getText().toString())){
+                    int duration = Toast.LENGTH_SHORT;
+                    CharSequence text = "Correo correcto";
+                    if (correo.getText().toString().equals("admin@utp.edu.pe") && contra.getText().toString().equals("admin")){
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        text = "Correo y contraseña incorrectos";
+                        Toast toast = Toast.makeText(LoginActivity.this, text, duration);
+                        toast.show();
+                    }
+                }
+                else{
+                    int duration = Toast.LENGTH_SHORT;
+                    CharSequence text = "Ingrese un correo valido";
+                    Toast toast = Toast.makeText(LoginActivity.this, text, duration);
+                    toast.show();
+                }
             }
         });
 
